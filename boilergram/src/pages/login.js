6a8b1a9bb29a849369da3a472d2380db2d5e5aa8
GameShiba <zhan4138@purdue.edu>
2023-02-26 23:07:35 -0500

    Complete Login Functionality with Firebase

--- boilergram/src/pages/login.js
+++ boilergram/src/pages/login.js
@@ -1,6 +1,7 @@
 import {useState, useContext, useEffect} from 'react';
 import {useHistory} from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Login() {
   const history = useHistory();
@@ -12,7 +13,18 @@ export default function Login() {
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleLogin = () => {};
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      history.push(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAddress('');
+      setPassword('');
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = 'Login - Boilergram';
@@ -23,8 +35,51 @@ export default function Login() {
       <div className='flex w-3/5'>
         <img src="/images/boilergram.png" alt="Boilergram Icon"/>
       </div>
-      <div className='flex flex-col w-2/5'>
-        <p>I will be the form</p>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" alt="Boilergram" className="mt-2 w-6/12 mb-4"/>
+          </h1>
+
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2
+                         border border-gray-primary rounded mb-2"
+              onChange={({target}) => setEmailAddress(target.value)}
+              value={emailAddress}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2
+                         border border-gray-primary rounded mb-2"
+              onChange={({target}) => setPassword(target.value)}
+              value={password}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold 
+              ${isInvalid && 'opacity-50'}`}
+            >
+              Log in
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+          <p className="text-sm">
+            Don't have an account?{` `}
+            <a to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
+              Sign up
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
--- boilergram/tailwind.config.js
+++ boilergram/tailwind.config.js
@@ -1,10 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true
+  },
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    }
+  },
+  variants: {
+    extend: {
+      display: ['group-hover']
+    }
   },
   plugins: [],
 }
