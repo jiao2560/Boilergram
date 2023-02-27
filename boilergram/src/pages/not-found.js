1248c5fa41056e42c4fcefedfdc6dab1a2a6dc7d
Tongwei Zhang <xfytrfhr13@hotmail.com>
2023-02-27 00:26:48 -0500

    not found page refined

--- boilergram/src/pages/not-found.js
+++ boilergram/src/pages/not-found.js
@@ -1,5 +1,4 @@
 import {useEffect} from 'react';
-import Header from '../components/header';
 
 export default function NotFound() {
   useEffect(() => {
@@ -8,7 +7,6 @@ export default function NotFound() {
 
   return (
     <div className="bg-gray-background">
-      <Header />
       <div className="mx-auto max-w-screen-lg">
         <p className="text-center text-2xl">Not Found!</p>
       </div>
