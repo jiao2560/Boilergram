4c7320982fa2e4954c0a19ba121614611ede4e54
GameShiba <zhan4138@purdue.edu>
2023-03-02 00:39:55 -0500

    Update Not Found Page Functionality

--- boilergram/src/pages/not-found.js
+++ boilergram/src/pages/not-found.js
@@ -2,7 +2,7 @@ import {useEffect} from 'react';
 
 export default function NotFound() {
   useEffect(() => {
-    document.title = 'Not Found - boilergram';
+    document.title = 'Not Found - Boilergram';
   }, []);
 
   return (
