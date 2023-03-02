d1077cb4728456344de51e96ef1201aa8e0bcc06
GameShiba <zhan4138@purdue.edu>
2023-03-02 01:09:16 -0500

    Create user.js User Context

--- boilergram/src/hooks/use-auth-listener.js
+++ boilergram/src/hooks/use-auth-listener.js
@@ -2,17 +2,19 @@ import {useState, useEffect, useContext} from 'react';
 import FirebaseContext from '../context/firebase';
 
 export default function useAuthListener() {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
+  const [user, setUser] = useState(
+      JSON.parse(localStorage.getItem('authUser')),
+  );
   const {firebase} = useContext(FirebaseContext);
 
   useEffect(() => {
     const listener = firebase.auth().onAuthStateChanged((authUser) => {
       if (authUser) {
-        // we have a user...therefore we can store the user in localstorage
+        // Have a authUser -> store the user in localstorage
         localStorage.setItem('authUser', JSON.stringify(authUser));
         setUser(authUser);
       } else {
-        // we don't have an authUser, therefore clear the localstorage
+        // Don't have an authUser -> clear the localstorage
         localStorage.removeItem('authUser');
         setUser(null);
       }
