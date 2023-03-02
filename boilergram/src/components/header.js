a961814848953f8acc1a8d760fa2c0a76af04413
GameShiba <zhan4138@purdue.edu>
2023-03-02 02:09:40 -0500

    Update Header Component

--- boilergram/src/components/header.js
+++ boilergram/src/components/header.js
@@ -1,33 +1,26 @@
 import {useContext} from 'react';
-import {Link, useHistory} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import UserContext from '../context/user';
 import * as ROUTES from '../constants/routes';
-import {DEFAULT_IMAGE_PATH} from '../constants/paths';
-import useUser from '../hooks/use-user';
 
 export default function Header() {
-  const {user: loggedInUser} = useContext(UserContext);
-  const {user} = useUser(loggedInUser?.uid);
+  const {user} = useUser(UserContext);
   const {firebase} = useContext(FirebaseContext);
-  const history = useHistory();
 
   return (
     <header className="h-16 bg-white border-b border-gray-primary mb-8">
       <div className="container mx-auto max-w-screen-lg h-full">
         <div className="flex justify-between h-full">
-          <div className="text-gray-700 text-center flex items-center
-                          align-items cursor-pointer">
+          <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
             <h1 className="flex justify-center w-full">
-              <Link to={ROUTES.DASHBOARD} aria-label="Boilergram Logo">
-                <img src="/images/logo.png"
-                  alt="Boilergram" className="mt-2 w-6/12" />
+              <Link to={ROUTES.DASHBOARD} aria-label="Boilergram logo">
+                <img src="/images/logo.png" alt="Boilergram" className="mt-2 w-6/12" />
               </Link>
             </h1>
           </div>
-          <div
-            className="text-gray-700 text-center flex items-center align-items">
-            {loggedInUser ? (
+          <div className="text-gray-700 text-center flex items-center align-items">
+            {user ? (
               <>
                 <Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
                   <svg
@@ -41,24 +34,17 @@ export default function Header() {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth={2}
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2
-                        2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0
-                        011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                     />
                   </svg>
                 </Link>
-
                 <button
                   type="button"
                   title="Sign Out"
-                  onClick={() => {
-                    firebase.auth().signOut();
-                    history.push(ROUTES.LOGIN);
-                  }}
+                  onClick={() => firebase.auth().signOut()}
                   onKeyDown={(event) => {
                     if (event.key === 'Enter') {
                       firebase.auth().signOut();
-                      history.push(ROUTES.LOGIN);
                     }
                   }}
                 >
@@ -73,33 +59,26 @@ export default function Header() {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth={2}
-                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3
-                        3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                     />
                   </svg>
                 </button>
-                {user && (
-                  <div className="flex items-center cursor-pointer">
-                    <Link to={`/p/${user?.username}`}>
-                      <img
-                        className="rounded-full h-8 w-8 flex"
-                        src={`/images/avatars/${user?.username}.jpg`}
-                        alt={`${user?.username} profile`}
-                        onError={(e) => {
-                          e.target.src = DEFAULT_IMAGE_PATH;
-                        }}
-                      />
-                    </Link>
-                  </div>
-                )}
+                <div className="flex items-center cursor-pointer">
+                  <Link to={`/p/${user.displayName}`}>
+                    <img
+                      className="rounded-full h-8 w-8 flex"
+                      src={`/images/avatars/${user.displayName}.jpg`}
+                      alt={`${user.displayName} profile`}
+                    />
+                  </Link>
+                </div>
               </>
             ) : (
               <>
                 <Link to={ROUTES.LOGIN}>
                   <button
                     type="button"
-                    className="bg-blue-medium font-bold
-                      text-sm rounded text-white w-20 h-8"
+                    className="bg-blue-medium font-bold text-sm rounded text-white w-20 h-8"
                   >
                     Log In
                   </button>
@@ -107,8 +86,7 @@ export default function Header() {
                 <Link to={ROUTES.SIGN_UP}>
                   <button
                     type="button"
-                    className="font-bold text-sm rounded
-                      text-blue-medium w-20 h-8"
+                    className="font-bold text-sm rounded text-blue-medium w-20 h-8"
                   >
                     Sign Up
                   </button>
