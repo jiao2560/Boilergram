a82c8d9d945aa7f0915cc54d254a2ff95292e38a
jiao28 <82656091+jiao28@users.noreply.github.com>
2023-03-02 20:49:10 -0500

    Add files via upload

---
+++ boilergram/src/components/dashboard.js
@@ -0,0 +1,31 @@
+import {useEffect} from 'react';
+import Header from './header';
+import Timeline from './timeline';
+import Sidebar from './sidebar';
+
+export default function Dashboard() {
+  useEffect(() => {
+    document.title = 'Boilergram';
+  }, []);
+
+  return (
+    <div className="bg-gray-background">
+
+      <Header/>
+      <div className="grid grid-cols-3 gap-4 justify-between
+                      mx-auto max-w-screen-lg">
+        <Timeline/>
+        <Sidebar/>
+
+
+
+
+
+
+
+      </div>
+
+
+    </div>
+  );
+}
