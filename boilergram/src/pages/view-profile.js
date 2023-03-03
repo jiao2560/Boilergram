37d3c3984766377316dce2a1cc12706740f7e483
GameShiba <zhan4138@purdue.edu>
2023-03-03 01:56:01 -0500

    Revert "Update view-profile.js"
    This reverts commit 2e103aea7f5d11663d81d22cc52355c95ec21120.

--- boilergram/src/pages/view-profile.js
+++ boilergram/src/pages/view-profile.js
@@ -5,39 +5,25 @@ import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
 import { getAuth, updateProfile , updateEmail, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
 
+const auth = getAuth();
 
-
-export default function viewprofile() {
-  const [oldemail, setoldemail] = useState('');
+export default function ResetPassword() {
     const auth = getAuth();
 
-    const triggerResetEmail = async () => {
-      const auth = getAuth();
     const user = auth.currentUser;
     if (user != null) {
+      // The user object has basic properties such as display name, email, etc.
       const displayName = user.displayName;
       const email = user.email;
       const photoURL = user.photoURL;
+      const emailVerified = user.emailVerified;
+      const fullName = user.fullName;
+      const userID = user.userID;
+
+      // The user's ID, unique to the Firebase project. Do NOT use
+      // this value to authenticate with your backend server, if
+      // you have one. Use User.getToken() instead.
       const uid = user.uid;
-      if (oldemail == 'wbj') {
-      alert("username: wbj  email: jiao29@purdue.edu   uid: 10   full name: Wenbo Jiao  date created: Mar 2, 2023");
-
-      }else {
-        alert("username: zhan4138  email: zhan4138@purdue.edu   uid: 11   full name: Tansi Zhang  date created: Mar 1, 2023");
-      }}
-  }
-
-
-    return (
-      <div className="flex justify-center items-center flex-col w-fullbg-white p-4 rounded border border-gray-primary">
-         <p className="text-sm">
-
-
-         <input className="resetEmailInput1" placeholder="input username" name = "oldemail" type="text" value={oldemail}
-        onChange={e => setoldemail(e.target.value)} required/><br></br>
-
-       <button className="resetBtn" type="button" onClick={triggerResetEmail}>search user</button>
-      </p>
-     </div>
-     )
+      alert("username:"+displayName + "     " + "email:" +email);
+    }
 }
